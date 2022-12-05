c18a9ee947477f2760368d531633e66eb1172950
Paul K <64181231+BVC414800@users.noreply.github.com>
2022-12-05 15:27:03 -0700

    SODV 2201 Term Project  Files
    These are files contained in the backend (server) folder

---
+++ register.js
@@ -0,0 +1,8 @@
+/*
+    SODV 2201 Web Programming Term Project
+      Displaying data from backend (server) to the frontend (client) with React JS.
+      This program is designed and coded by SODV 2201 Section 1 Group 3.
+      Design and coding by :  Candace Arriola and Paul Kho
+      Instructor           :   Tigis Beshe
+      Due                  :   December 12 2022  
+*/
